02ca19764233b7b9756210d0aab2eca7cd840234
Jacob Adams <Toukmond@users.noreply.github.com>
2014-09-18 10:07:47 -0400

    create header

---
+++ BSDsocket.h
@@ -0,0 +1,21 @@
+#ifndef _BSDSOCKET_H_
+#define _BSDSOCKET_H_
+
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+struct hostent *sock_gethostbyname(char *name);
+int sock_listener(unsigned short portnum,int max_connect);
+int sock_getconnection(int s);
+int sock_connectto(char *hostname,unsigned short portnum);
+int sock_read(int s, char *buf, int n);
+int sock_write(int s, char *buf, int n);
+
+#endif
