99fbda106001a120a61c8830d04baca377618535
Jacob Adams <Toukmond@users.noreply.github.com>
2014-09-18 10:22:56 -0400

    Create a simple test

---
+++ test.c
@@ -0,0 +1,22 @@
+/* Simple test program */
+#include <stdio.h>
+#include <stdlib.h>
+#include "BSDsocket.h"
+
+#define PORT 8888
+#define MESSAGE "Hello World!!"
+
+int main () {
+	int listener = sock_listener(PORT,1);
+	int client = sock_connectto("127.0.0.1",PORT);
+	int server = sock_connection(listener);
+	
+	char *message = MESSAGE;
+	printf("Sending: %s",message);
+	sock_write(client,message,sizeof(message));
+	char recivebuf[sizeof(message)+1]; /* just to be safe... */
+	memset(recivebuf,0,sizeof(recivebuf));
+	sock_read(server,recivebuf,sizeof(message));
+	printf("Recived: %s",recivebuf);
+	return 0;
+}
