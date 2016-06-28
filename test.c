90434f4c23101b4620a7bd9cd94a8756c91bc3d7
Jacob Adams <tookmund@gmail.com>
2016-06-28 15:26:50 -0400

    test: sizeof c buf does not work properly

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 
 #define PORT 8888
 #define MESSAGE "Hello World!!"
-
+#define MESSLEN 14
 int main () {
 	int listener = sock_listener(PORT,1);
 	printf("listener created\n");
@@ -15,10 +15,10 @@ int main () {
 	int server = sock_accept(listener);
 	char *message = MESSAGE;
 	printf("\nSending: %s\n",message);
-	sock_write(client,message,sizeof(message));
-	char recivebuf[sizeof(message)+1]; /* just to be safe... */
-	memset(recivebuf,0,sizeof(recivebuf));
-	sock_read(server,recivebuf,sizeof(message));
+	sock_write(client,message,MESSLEN);
+	char recivebuf[MESSLEN+1]; /* just to be safe... */
+	memset(recivebuf,0,MESSLEN+1);
+	sock_read(server,recivebuf,MESSLEN);
 	printf("\nRecived: %s\n",recivebuf);
 	return 0;
 }
